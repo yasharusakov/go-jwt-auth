9d1aebf2308fc099bf8dd20e5ad2794da8626daf
yasharusakov <rusakov.yasha@gmail.com>
2026-01-25 14:13:42 +0200

    refactor(auth): improve handler and DTO separation

---
+++ backend/auth-service/internal/dto/request_dto.go
@@ -0,0 +1,11 @@
+package dto
+
+type RegisterRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
